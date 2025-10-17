a2aa7d662ec312c62490f0abd5f1119050f956a7
kruparaoKapil <krupagarao.g@kapilit.com>
2025-10-17 16:29:49 +0530

    removeed local servicekey

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 const { Server } = require("socket.io");
 const app = express();
 const admin = require("firebase-admin");
-// const serviceAccount = require("./serviceAccountKey.json");
 const serviceAccount = require("/home/bitnami/config/serviceAccountKey.json");
 
 
